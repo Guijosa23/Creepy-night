21c25439706c6122c127697404082cf139e2d1a9
Guijosa23 <mariguij553@ed.amdsb.ca>
2017-11-08 13:29:21 -0500

    Add project files.

---
+++ WindowsFormsApp12/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Gray;
+            this.outputLabel.Font = new System.Drawing.Font("Papyrus", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(42, 21);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(518, 164);
+            this.outputLabel.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox1.BackgroundImage = global::WindowsFormsApp12.Properties.Resources.png;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(46, 368);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 55);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::WindowsFormsApp12.Properties.Resources.png2;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(46, 320);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(51, 42);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp12.Properties.Resources.forest;
+            this.ClientSize = new System.Drawing.Size(598, 489);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ WindowsFormsApp12/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        int scene = 1;
+
+        Random randGen = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            double diceValue = randGen.Next(1, 101);
+
+            if (e.KeyCode == Keys.R)
+            {
+                if (scene == 1) { scene = 2; }
+                else if (scene == 3)
+                {
+                    if (diceValue <= 70)
+                    {
+                        scene = 3;
+                    }
+                    else { scene = 2; }
+                }
+                else if (scene == 2) { scene = 5; }
+                else if (scene == 6) { scene = 7; }
+                else if (scene == 8) { scene = 9; }
+                else if (scene == 10) { scene = 11; }
+                else if (scene == 12) { scene = 15; }
+                else if (scene == 15) { scene = 17; }
+                else if (scene == 16) { scene = 19; }
+
+            }
+                       
+            if (e.KeyCode == Keys.Y)
+            {
+                if (scene == 1) { scene = 3; }
+                else if (scene == 2)
+                {
+                    if (diceValue <= 70)
+                    {
+                        scene = 2;
+                    }    
+                    else { scene = 3; }
+                }     
+                else if (scene == 2) { scene = 4; }
+                else if (scene == 6) { scene = 8; }
+                else if (scene == 8) { scene = 10; }
+                else if (scene == 10) { scene = 12; }
+                else if (scene == 12) { scene = 14; }
+                else if (scene == 15) { scene = 16; }
+                else if (scene == 16) { scene = 18; }
+
+
+
+
+
+            }
+        }
+    }
+}
